712f9aaa95d7f38318bd3762717d165f782e2b58
Asrafuddinbasir55 <asrafuddinbasir0@gmail.com>
2022-05-30 03:50:43 +0600

    third commit

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -11,17 +11,17 @@ import Email from '../assets/icons/email.png'
 const Portfolio = () => {
     return (
         <div>
-            <div className=" card lg:card-side  bg-orange-300 shadow-xl">
+            <div className=" card lg:card-side  bg-blue-300 shadow-xl">
                 <figure><img className='' src={image} alt="Album" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title text-primary text-4xl font-bold">This is Shorif Uddin</h2>
+                    <h2 className="card-title text-pink text-4xl font-bold">My Name is Asraf Uddin Basir</h2>
                     <p className='w-80 text-1xl'>I'm Proportional Full Stack Web Developer.My profession is heard worked coding.2022 year Full Stack Project in the website bootstrap,tailwind, react bootstrap, es6, react router, firebase, mongodb, node js, heroku,express js. etc my product building very awesome  locking for  website.</p>
                     
                 </div>
             </div>
             <section>
 
-                <div className=' card bg-orange-200 shadow-2xl p-5'>
+                <div className=' card bg-indigo-200 shadow-2xl p-5'>
                     <h1 className='text-primary text-5xl font-bold text-center p-5'>This is My Best website</h1>
                     <div className='grid grid-cols-3 gap-4 '>
 
@@ -35,9 +35,9 @@ const Portfolio = () => {
                                 <p>React Bootstrap, Node Js, Mongodb, Firebase, Es6, Express js,Heroku, .env etc My website building</p>
                                 <div className="card-actions justify-end">
                                     <div className='w-50'>
-                                        <a href='https://github.com/ProgrammingHeroWC4/warehouse-management-client-side-shorifuddin2' target='_blank' className="badge badge-outline">Cline Github</a>
-                                        <a href='https://github.com/ProgrammingHeroWC4/warehouse-management-server-side-shorifuddin2' target='_blank' className="badge badge-outline">Server Github</a>
-                                        <a href='https://smartphone-show-room-88deb.web.app/' target='_blank' className="badge badge-outline">Live website</a>
+                                        <a href='https://github.com/Asrafuddinbasir55' target='_blank' className="badge badge-outline">Cline Github</a>
+                                      
+                                        <a href='https://awesome-clarke-32d4fe.netlify.app/' target='_blank' className="badge badge-outline">Live website</a>
                                     </div>
                                 </div>
                             </div>
@@ -53,9 +53,9 @@ const Portfolio = () => {
                                 <p>Bootstrap, firebase, netlify, es6, Node Js, Mongodb</p>
                                 <div className="card-actions justify-end">
                                     <div >
-                                        <a href='https://github.com/programming-hero-web-course-4/product-analysis-website-shorifuddin2' target='_blank' className="badge badge-outline">Github Link</a>
+                                        <a href='https://github.com/Asrafuddinbasir55' target='_blank' className="badge badge-outline">Github Link</a>
 
-                                        <a href='https://velvety-figolla-d9db1f.netlify.app/
+                                        <a href='https://tangerine-vacherin-6f675f.netlify.app/
                                     ' target='_blank' className="badge badge-outline">Live website</a>
                                     </div>
                                 </div>
@@ -73,9 +73,9 @@ const Portfolio = () => {
                                 <p>Bootstrap, firebase, es6, Node Js, Mongodb</p>
                                 <div className="card-actions justify-end">
                                     <div >
-                                        <a href='https://github.com/programming-hero-web-course-4/independent-service-provider-shorifuddin2' target='_blank' className="badge badge-outline">Github Link</a>
+                                        <a href='https://github.com/Asrafuddinbasir55' target='_blank' className="badge badge-outline">Github Link</a>
 
-                                        <a href='https://tourist-man.web.app/
+                                        <a href='https://taupe-vacherin-e56bdd.netlify.app/
                                     ' target='_blank' className="badge badge-outline">Live website</a>
                                     </div>
                                 </div>
@@ -122,10 +122,10 @@ const Portfolio = () => {
                 </div>
                 <div className='mx-20 p-20'>
                         <h1 className='text-3xl'>Present Address</h1>
-                        <h2 className='text-2xl'>Email: shorifuddin240915@gmail.com</h2>
+                        <h2 className='text-2xl'>Email: asrafuddinbasir0@gmail.com</h2>
                         <h2 className='text-2xl'>District: Habiganj Sadar, Sylhet</h2>
                         <h2 className='text-2xl'>Nationality: Bangladeshi</h2>
-                        <h2 className='text-2xl'>Phone: +08801923-062919</h2>
+                        <h2 className='text-2xl'>Phone: +08801301745205</h2>
                         <div className='flex'>
                         <a href='https://www.facebook.com/shorif.uddin.milon2' target='_blank'><img className='w-8 m-3' src={Facebook}/></a>
                         <a href='https://github.com/shorifuddin2' target='_blank'><img className='w-8 m-3' src={Github}/></a>
